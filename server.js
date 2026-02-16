cc17f979fe319685828992d054343be26a4c0a0b
genzeedev-git <genzeedev@gmail.com>
2026-02-16 18:00:04 +0530

    deployment changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // CORS Configuration
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
+    origin: process.env.ALLOWED_ORIGIN || ['http://localhost:5173', 'http://localhost:3000'],
     credentials: true
 };
 
